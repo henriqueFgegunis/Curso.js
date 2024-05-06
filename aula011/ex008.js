71c1ed0120ff50a27eecccd60356097dcf248a6a
henriqueFgegunis <henriquefernandesgegunis@gmail.com>
2024-05-05 21:53:41 -0300

    save

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60) {
+    console.log(`voce ultrapassou a velocidade permitida.MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!.`)
