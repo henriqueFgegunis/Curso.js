1000d48815452d6cdb9bc024454e7beafea265b5
henriqueFgegunis <henriquefernandesgegunis@gmail.com>
2024-07-07 01:28:47 -0300

    save

--- aula012/ex012.js
+++ aula012/ex012.js
@@ -1,14 +1,14 @@
 var agora = new Date()
 var hora = agora.getHours()
 var minutos = agora.getMinutes()
-console.log(`agora sao exatamente ${hora} hrs e ${minutos} minutos`)
-if (hora < 12) {
+console.log(`agora sao exatamente ${hora}:${minutos} hrs`)
+if (hora >= 6) {
     console.log(`bom dia...`)
 }
-else if (hora <= 18) {
+else if (hora >= 18) {
     console.log(`boa tarde...`)
 }
-else if  (hora < 1) {
+else if  (hora > 0 || hora <= 5) {
     console.log(`boa madrugada...`)
 }
 else  {
