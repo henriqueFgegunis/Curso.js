5b573a2d47839e6ee0bc0becc6faddae13ce6e45
henriqueFgegunis <henriquefernandesgegunis@gmail.com>
2024-07-07 23:31:36 -0300

    Modulo E Repetiçao

--- aula013/ex015/script.js
+++ aula013/ex015/script.js
@@ -3,11 +3,56 @@ var data = new Date()
 var ano = data.getFullYear()
 var fano = document.getElementById('txtano')
 var res = document.getElementById('res')
-if (fano.value.length == 0 || fano > ano) {
+if (fano.value.length == 0 || Number(fano.value) > ano) {
  window.alert(`[ERRO] verifique os dados e tente novamente...`)
     } else {
-       window.alert('tudo ok')
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      res.innerHTML = `idade calculada ${idade}`
+      var genero = ''
+      var img = document.createElement('img')
+      img.setAttribute('id', 'foto')
+      if (fsex[0].checked) {
+        genero = 'Homem'
+
+        if (idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'bebehomem.png')
+        } else if (idade < 21) {
+            //jovem
+            img.setAttribute('src', 'homemjovem.png')
+        } else if (idade < 50) {
+            //adulto
+            img.setAttribute('src', 'homemadulto.png')
+        }else  {
+            //idoso
+            img.setAttribute('src', 'idosohomem.png')
+        }
+      } else if (fsex[1].checked) {
+        genero = 'Mulher'
+
+        if (idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'bebemulher.png')
+        } else if (idade < 21) {
+            //jovem
+            img.setAttribute('src', 'mulherjovem.png')
+        } else if (idade < 50) {
+            //adulto
+            img.setAttribute('src', 'mulheradulta.png')
+        }else  {
+            //idoso
+            img.setAttribute('src', 'idosamulher.png')
+        }
+
+      }
+     res.style.textAlign = 'center'
+     res.innerHTML = `Detectamos ${genero} com ${idade} anos...`
+     res.appendChild(img)
+     
+      
     }
+    
     
 }
 
