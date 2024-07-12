20ecd5deea7b3d0a453ac1c160ef3ed120203295
henriqueFgegunis <henriquefernandesgegunis@gmail.com>
2024-07-12 07:39:23 -0300

    Update script.js

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,4 +1,41 @@
 function contar() {
+    let ini = document.getElementById('txti');
+    let fim = document.getElementById('txtf');
+    let passo = document.getElementById('txtp');
+    let res = document.getElementById('res');
+  
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+      res.innerHTML = 'Impossível contar!';
+    } else {
+      res.innerHTML = 'Contando: <br>';
+      let i = Number(ini.value);
+      let f = Number(fim.value);
+      let p = Number(passo.value);
+      if (p >= 0) {
+        alert('passo 0 ou 0- invalido consideramos 0 igual a 1')
+        p = 1
+      }
+      if (p <= 0) {
+        window.alert('Passo inválido! Considerando PASSO 1');
+        p = 1;
+      }
+  
+      if (i < f) {
+        // Contagem crescente
+        for (let c = i; c <= f; c += p) {
+          res.innerHTML += `${c} \u{1F449}`;
+        }
+      } else {
+        // Contagem regressiva
+        for (let c = i; c >= f; c -= p) {
+          res.innerHTML += `${c} \u{1F448}`;
+        }
+      }
+  
+      res.innerHTML += '\u{1F3C1}';
+    }
+  }
+/*function contar() {
     const ini = document.getElementById('txti').value;
     const fim = document.getElementById('txtf').value;
     const passo = document.getElementById('txtp').value;
@@ -12,14 +49,19 @@ function contar() {
         const f = Number(fim);
         const p = Number(passo);
         if (i < f) {
-            for (let c = i; c <= f; c += p)
-                res.innerHTML += `${c} + ${p} \u{27A1} `;
-
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} + ${p} \u{27A1} `;}
+                //contagem regresiva
+        } else{
+            for (let c = i;c >= f; c -= p) {
+                res.innerHTML = `${c}\u{1f449}`;
+            }
         }
 
     }
 
 }
+*/
 /*function contar() {
     var ini = document.getElementById('txti');
     var fim = document.getElementById('txtf');
